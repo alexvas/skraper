6132b9d435b32b870f5dbcac9dd0ced70a309710
Александр Васильев <a.a.vasiljev@gmail.com>
2021-12-22 08:04:54 +0700

    ещё поправил замену ссылок

--- src/main/kotlin/aikisib/mirror/RecursiveScraper.kt
+++ src/main/kotlin/aikisib/mirror/RecursiveScraper.kt
@@ -3,6 +3,7 @@ package aikisib.mirror
 import aikisib.model.OriginalDescription
 import aikisib.url.UrlRelativizer
 import aikisib.url.UrlTransformer
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
@@ -35,41 +36,41 @@ internal class RecursiveScraperImpl(
 ) : RecursiveScraper {
     private val linkRepo: MutableMap<URI, Unit> = ConcurrentHashMap()
 
+    private val transformCache: MutableMap<OriginalDescription, URI> = ConcurrentHashMap()
+    private val descriptionRepo: MutableMap<URI, OriginalDescription> = ConcurrentHashMap()
+    private val fanOutRepo: MutableMap<OriginalDescription, Map<String, URI>> = ConcurrentHashMap()
+
     override suspend fun mirror() {
-        downloadItem(fromRoot)
+        coroutineScope {
+            downloadItem(this, fromRoot)
+        }
+        moveContentTransforming()
     }
 
-    private suspend fun downloadItem(from: URI) {
-        coroutineScope {
-            launch(Dispatchers.Default) {
-                doDownloadItem(from)
-            }
+    private suspend fun downloadItem(scope: CoroutineScope, from: URI) {
+        scope.launch(Dispatchers.Default) {
+            doDownloadItem(scope, from)
         }
     }
 
-    private suspend fun doDownloadItem(from: URI) {
+    private suspend fun doDownloadItem(scope: CoroutineScope, from: URI) {
         val originalDescription = downloader.download(from) ?: return
+        descriptionRepo[originalDescription.remoteUri] = originalDescription
+        transformCache[originalDescription] = transform(originalDescription)
         val links = linkExtractor.extractLinks(originalDescription)
         val filteredLinks = links.filter { fromLinkFilter.filter(it.value) }
         if (filteredLinks.isEmpty()) {
             moveFileInPlace(originalDescription)
             return
         }
+        fanOutRepo[originalDescription] = filteredLinks
 
         for ((_, uri) in filteredLinks) {
             val prev = linkRepo.put(uri, Unit)
             val notSeen = prev == null
             if (notSeen)
-                downloadItem(uri)
+                downloadItem(scope, uri)
         }
-
-        transformingMoveFileInPlace(
-            originalDescription = originalDescription,
-            relativeLinks = filteredLinks.mapValues { (_, uri) ->
-                relativizer.relativize(from, uri)
-            },
-            resolveTarget = resolveTarget(originalDescription),
-        )
     }
 
     private fun moveFileInPlace(originalDescription: OriginalDescription) {
@@ -79,9 +80,28 @@ internal class RecursiveScraperImpl(
     }
 
     private fun resolveTarget(originalDescription: OriginalDescription): Path {
-        val transformed = urlTransformer.transform(originalDescription.type, originalDescription.remoteUri)
+        val transformed = transformCache[originalDescription] ?: error("трансформация для $originalDescription не закэширована.")
         val relative = relativizer.relativize(fromRoot, transformed)
         return toRoot.resolve(relative.toString())
+    }
+
+    private fun transform(originalDescription: OriginalDescription) =
+        urlTransformer.transform(originalDescription.type, originalDescription.remoteUri)
+
+    private fun moveContentTransforming() {
+        for ((originalDescription, filteredLinks: Map<String, URI>) in fanOutRepo) {
+            val transformedUri = transformCache[originalDescription] ?: error("трансформация для $originalDescription не закэширована.")
+
+            transformingMoveFileInPlace(
+                originalDescription = originalDescription,
+                relativeLinks = filteredLinks.mapValues { (_, uri) ->
+                    val targetDescription = descriptionRepo[uri] ?: return@mapValues uri
+                    val transformedLink = transformCache[targetDescription] ?: error("целевая трансформация для $targetDescription не закэширована.")
+                    relativizer.relativize(transformedUri, transformedLink)
+                },
+                resolveTarget = resolveTarget(originalDescription),
+            )
+        }
     }
 
     private fun transformingMoveFileInPlace(
